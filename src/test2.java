ce96220702e2d4031c8e0082798ce8a559455c5d
Evilous10101 <43018063+Evilous10101@users.noreply.github.com>
2018-09-05 19:45:32 -0400

    Add files via upload

--- src/test2.java
+++ src/test2.java
@@ -1,4 +1,14 @@
 
 public class test2 {
 
+
+	private String name = "54";
+	
+	public static void main(String[] args)
+	{
+	System.out.println("insert random text here");
+	System.out.println("test complete");
+	System.out.println("It works");
+	}
 }
+
