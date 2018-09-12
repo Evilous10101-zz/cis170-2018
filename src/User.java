b173239530dab59d99d417c40cb938c743540afb
Evilous10101 <43018063+Evilous10101@users.noreply.github.com>
2018-09-12 19:08:11 -0400

    Add files via upload

---
+++ src/User.java
@@ -0,0 +1,45 @@
+
+public class User
+{
+	private String Username;
+	private String Email;
+	private String Firstname;
+	private String Lastname;
+	//Method signture consits of
+	//Accessor - public
+	//Return type - any type or boid
+	//Method name - make it good so people know what the method does
+	// Argument list - parameters passed to the method
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String Username) {
+		this.Username = Username;
+	}
+	public String getEmail() {
+		return Email;
+	}
+			public void setEmail(String Email) {
+				this.Email = Email;
+			}
+			public String getFirstname() {
+				return Firstname;
+			}
+			public void setFirstname(String Firstname) {
+				this.Firstname = Firstname;
+			}
+			public String setLastname() {
+				return Lastname;
+			}
+			public void setLastname(String Lastname) {
+				this.Lastname = Lastname;
+			}
+			
+		
+	
+				
+	
+	}
+	
+	
+
